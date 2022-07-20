f628c95fdb9befd6e6a8a0afdd8c9beda44c2278
Jérôme Schneider <jschneider@cloudflare.com>
2022-07-20 19:16:03 +0200

    Fun with workers!

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use text_to_png::{TextPng, TextRenderer};
+use worker::*;
+
+mod utils;
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+
+    let router = Router::new();
+
+    router
+        .get_async("/", |req, _| async move { 
+            if let Some(text) = req.url()?.query() {
+                handle_slash(text.into()).await
+            } else {
+                handle_slash("What's up Cloudflare".into()).await
+            }
+        })
+        .run(req, env)
+        .await
+}
+
+async fn handle_slash(text: String) -> Result<Response> {
+    let renderer = TextRenderer::default();
+
+    let text = if text.len() > 128 {
+        "Nope".into()
+    } else {
+        text
+    };
+
+    let text_png: TextPng = renderer
+        .render_text_to_png_data(text, 64, "red")
+        .unwrap();
+
+    let mut headers = Headers::new();
+    headers.set("content-type", "image/png")?;
+
+    Ok(Response::from_bytes(text_png.data)?.with_headers(headers))
+}
